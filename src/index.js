ead589470412b36670527f82d8f805734993e002
marcos <mcsar1997@gmail.com>
2020-04-05 22:27:15 -0300

    feat: Add axios to make api requests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// react
 import React from 'react';
+
+// third-party
+import axios from 'axios';
 import ReactDOM from 'react-dom';
+
+// aplication
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = 'https://api.github.com';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
