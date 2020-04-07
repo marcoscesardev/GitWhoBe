b78ff522c3cdb6868b9637cda346481a2f90b31b
marcos <mcsar1997@gmail.com>
2020-04-07 00:38:54 -0300

    feat: Add Home page form with axios resquest

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,7 +1,14 @@
 // react
-import React from 'react';
+import React, { useState } from 'react';
+
+// third-party
+import axios from 'axios';
+import { Formik } from 'formik';
+import classNames from 'classnames';
 
 export default function HomePage() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="home-page puff-in-center">
       <div className="home-page__form">
@@ -9,6 +16,85 @@ export default function HomePage() {
           <h1>GitWhoBe</h1>
           <span>Descubra informações de usuários do GitHub</span>
         </div>
+
+        <Formik
+          initialValues={{ username: '' }}
+          validate={values => {
+            const errors = {};
+
+            if (!values.username) {
+              errors.username = 'Obrigatório';
+            } else if (!/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}[a-z\d]$/i.test(values.username)) {
+              errors.username = `Pode conter apenas caracteres alfanuméricos ou hífens únicos e não
+                pode começar ou terminar com um hífen.`;
+            }
+
+            return errors;
+          }}
+
+          onSubmit={(values, { setSubmitting, setFieldError, resetForm }) => {
+            axios
+              .get(`/users/${values.username}`)
+              .then(({ data }) => {
+                setSubmitting(false);
+                setUser(data);
+                resetForm();
+              })
+              .catch(error => {
+                setSubmitting(false)
+                if (error.response === 404) {
+                  setFieldError(
+                    'username',
+                    'Este usuário não existe!'
+                  );
+                } else {
+                  setFieldError(
+                    'username',
+                    'Ocorreu um erro na requisição'
+                  );
+                }
+              });
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit} className="">
+              <div className="home-page__form-group">
+                <div className="home-page__icon">@</div>
+                <input
+                  className={classNames('home-page__input', {
+                    'home-page__input--error': errors.username,
+                  })}
+                  type="username"
+                  name="username"
+                  placeholder="Usuário"
+                  onChange={handleChange}
+                  value={values.username}/>
+
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={classNames('btn', {
+                    'btn--disabled': isSubmitting,
+                  })}
+                >
+                  BUSCAR
+                </button>
+              </div>
+
+              <div className="home-page__message--error">
+                {errors.username && touched.username && errors.username}
+              </div>
+
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   )
