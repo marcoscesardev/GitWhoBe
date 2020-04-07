8f989eccb724066965bc58df0b6d8e919d76a7b5
marcos <mcsar1997@gmail.com>
2020-04-06 23:22:12 -0300

    Updates

---
+++ precache-manifest.7ef4cc6cde30093ac79be2d91803f523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b48b2486ff08fe6deee3592be238c0a",
+    "url": "/GitWhoBe/index.html"
+  },
+  {
+    "revision": "a63392450e9d011afde3",
+    "url": "/GitWhoBe/static/css/main.75bc2cbe.chunk.css"
+  },
+  {
+    "revision": "48a31f4e639df27c5ea5",
+    "url": "/GitWhoBe/static/js/2.a4f56199.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/GitWhoBe/static/js/2.a4f56199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63392450e9d011afde3",
+    "url": "/GitWhoBe/static/js/main.efc5b972.chunk.js"
+  },
+  {
+    "revision": "bf27e80c0b18cf4b32e6",
+    "url": "/GitWhoBe/static/js/runtime-main.cdeb2357.js"
+  }
+]);
