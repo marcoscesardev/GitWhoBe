215334cdb14384267b510817f71bcf94776eff7e
marcos <mcsar1997@gmail.com>
2020-04-07 00:20:21 -0300

    feat: Add home page and route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+// React
 import React from 'react';
-import './App.scss';
+
+// tirt-parts
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+// Application
+import HomePage from './pages/Home/HomePage'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,15 @@
+// react
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="home-page puff-in-center">
+      <div className="home-page__form">
+        <div className="home-page__title">
+          <h1>GitWhoBe</h1>
+          <span>Descubra informações de usuários do GitHub</span>
+        </div>
+      </div>
+    </div>
+  )
+}
